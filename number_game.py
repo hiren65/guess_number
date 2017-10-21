7dd83a0c83a4fbd9638aba556d65034f554f7eb4
Hirenkumar Patel <hirenkumarpatel@Hirenkumars-MacBook-Pro.local>
2017-10-21 18:19:29 +1100

    we added guess number greater than and less than secreate number

--- number_game.py
+++ number_game.py
@@ -7,9 +7,9 @@ count = 0
 
 while (count<5):
 
-    count = count + 1
 
-    Guess_number = input("What is your number :")
+
+    Guess_number = input("Guess between 1 to 10, What is your number :")
     if Guess_number == "show":
         print(secreate_number)
         continue
@@ -20,9 +20,10 @@ while (count<5):
         print("Wrong Input, Please enter integer")
         continue
     Int_Guess = int(Guess_number)
+    count = count + 1
     if secreate_number == (Int_Guess):
-        print("Greate your guess number {} match with secrate number {}".format(Guess_number,secreate_number))
-        moderator = input("you want yo play again? answer yes or no!! ")
+        print("Greate, You win, your guess number {} match with secrate number {}".format(Guess_number,secreate_number))
+        moderator = input("you want to play again? answer yes or no!! ")
         if moderator == "yes":
             count = 0
             secreate_number = random.randint(1,10)
@@ -32,14 +33,19 @@ while (count<5):
         break
     else:
         if count == 5:
-            print("Sorry your trial excceed 5 times")
-            moderator = input("you wan to play again? answer in yes or no! ")
+            print("Sorry your trial excceed 5 times and your secreat randum number was {}".format(Guess_number))
+            moderator = input("you want to play again? answer in yes or no! ")
             if moderator=="yes":
                 count = 0
                 secreate_number = random.randint(1, 10)
                 continue
             else:
                 print("Bye Bye!!!")
-        print("Sorry you miss it, try again")
+        if secreate_number<Int_Guess:
+            print("Sorry, You guess is greater than secreate no, you miss it!")
+            #print("Sorry you miss it, try again")
+        if secreate_number>Int_Guess:
+            print("Sorry, You guess is less than secreate no, you miss it!")
+            #print("Sorry you miss it, try again")
         continue
 
